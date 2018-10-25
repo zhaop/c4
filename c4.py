01008a42971dff2c7018d55f6cd1a4b3ddeea9d5
Patrick Zhao <zhaopatrick@gmail.com>
2018-10-25 17:26:04 +0200

    Implement unplay to make hypothetical moves nicer

--- c4.py
+++ c4.py
@@ -8,7 +8,7 @@ from timeit import default_timer as timer
 class Game:
 
 	def __init__(self, nc=7, nr=6, n=4):
-		self.P = ['.', 'x', 'o', '-']
+		self.P = ['.', '🔵', '🔴', '-']
 		self.nc, self.nr, self.n = nc, nr, n
 		self.clear()
 
@@ -23,15 +23,23 @@ class Game:
 		return len(col) - 1
 
 	def canPlay(self, c):
-		'Return whether play would be valid'
+		'Return whether move would be legal'
 		return not self.over() and 0 <= c < self.nc and self.topFree(self.cols[c]) >= 0
 
 	def play(self, c):
-		'Play move'
+		'Play move c'
 		r = self.topFree(self.cols[c])
 		assert(r >= 0)
 		self.cols[c][r] = self.P[self.p]
 		self.winner = self.calcWinner(c)
+		self.p = {1: 2, 2: 1}[self.p]
+
+	def unplay(self, c):
+		'Unplay move c'
+		r = self.topFree(self.cols[c]) + 1
+		assert(r >= 0)
+		self.winner = 0
+		self.cols[c][r] = self.P[0]
 		self.p = {1: 2, 2: 1}[self.p]
 
 	def over(self):
@@ -43,6 +51,10 @@ class Game:
 			yield self.cols[c][r]
 
 	def calcWinner(self, c0):
+		'''
+		Return who wins given that the current player played at c0 (call AFTER c0 is placed)
+		0: nobody, 1: player 1, 2: player 2, 3: ends in a draw
+		'''
 		r0 = self.topFree(self.cols[c0]) + 1
 		me = self.cols[c0][r0]
 
@@ -92,21 +104,21 @@ class Game:
 		if n >= self.n: return self.p
 
 		# Check for draw
-		if r0 == 0 and all(c != self.P[0] for c in self.row(0)):
+		if r0 == 0 and all(v != self.P[0] for v in self.row(0)):
 			return 3
 
 		return 0
 
 	def __repr__(self):
-		s = ' '.join(map(str, range(self.nc))) + '\n'
-		s += '\n'.join(' '.join(col[r] for col in self.cols) for r in range(self.nr))
-
-		ss = ['Turn (%s)' % self.P[self.p]]
-		if self.winner != 0: ss += ['Winner (%s)' % self.P[self.winner]]
+		ss = [] if self.winner else ['Turn %s' % self.P[self.p]]
+		if self.winner != 0: ss += ['Winner %s' % self.P[self.winner]]
 		if self.over(): ss += ['Game over']
 
-		s += '\n' + '\t'.join(ss)
-		return s + '\n'
+		return '\n'.join([
+			'\t'.join(ss),
+			' '.join(map(str, range(self.nc))),
+			'\n'.join(' '.join(col[r] for c, col in enumerate(self.cols)) for r in range(self.nr))
+		])
 
 	def copyFrom(self, other):
 		for c in range(self.nc):
@@ -118,25 +130,29 @@ class Game:
 	def actionSpace(self):
 		return list(c for c in range(self.nc) if self.cols[c][0] == self.P[0])
 
-# Bad move: if played, the opponent has a sure win move afterwards (so don't play it)
-def badMoves(g, g2, g3):
-	bad = set()
-	me = g.p
-	for c in g.actionSpace():
-		g2.copyFrom(g)
+# List of all valid moves except bad moves (which if played, the opponent has a sure win move afterwards)
+# Except if there is a sure win move for us, in which case return only that (if multiple, return one of them)
+# moves: if provided, will filter on a copy of this list instead
+def reasonableMoves(g, g2, *, moves=None):
+	if moves is None: moves = g.actionSpace()
+	moves, bad, me = set(moves), set(), g.p
+
+	g2.copyFrom(g)
+	for c in moves:
 		g2.play(c)
 		if g2.winner == me:
-			bad = g.actionSpace()
-			bad.remove(c)
-			return set(bad)
+			return [c]
 		opp = g2.p
 		for c2 in g2.actionSpace():
-			g3.copyFrom(g2)
-			g3.play(c2)
-			if g3.winner == opp:
+			g2.play(c2)
+			if g2.winner == opp:
 				bad.add(c)
+				g2.unplay(c2)
 				break
-	return bad
+			g2.unplay(c2)
+		g2.unplay(c)
+
+	return list(moves - bad)
 
 def human(*, name='Human'):
 	def f(g):
@@ -153,12 +169,10 @@ def smartNoise(*, name='Smart noise', verbose=True):
 		def out(c):
 			if verbose: print('%s: %s' % (name, c))
 			return c
-
-		g2, g3 = Game(), Game()
 		acs = g.actionSpace()
-		cs = list(set(acs) - badMoves(g, g2, g3))
-		cs = cs or acs
-		return out(choice(cs))
+		cs = reasonableMoves(g, Game(), moves=acs)
+		if verbose: print('Reasonable moves: %s' % cs)
+		return out(choice(cs or acs))
 
 	return f
 
@@ -181,12 +195,9 @@ def mcs(*, name='MCS', timeout=5.0, verbose=True):
 		totals = {c: 0 for c in cs}
 		wins = {c: 0 for c in cs}
 
-		g2, g3, g4 = Game(), Game(), Game()
+		g2, g3 = Game(), Game()
 
-		# Prune obviously bad moves 1 step ahead
-		cs = list(set(cs) - badMoves(g, g2, g3))
-		if verbose: print('After pruning: %s' % sorted(cs))
-
+		cs = reasonableMoves(g, g2, moves=cs)
 		if not cs: return out(choice(g.actionSpace()))
 		if len(cs) == 1: return out(cs[0])
 
@@ -198,64 +209,65 @@ def mcs(*, name='MCS', timeout=5.0, verbose=True):
 			g2.play(c1)
 
 			acs2 = g2.actionSpace()
-			cs2 = list(set(acs2) - badMoves(g2, g3, g4))
-			c2 = choice(cs2 if cs2 else acs2)
+			cs2 = reasonableMoves(g2, g3, moves=acs2)
 
-			g3.copyFrom(g2)
-			g3.play(c2)
+			c2 = choice(cs2 or acs2)
+			g2.play(c2)
 
-			while not g3.winner:
-				g3.play(choice(g3.actionSpace()))
+			while not g2.winner:
+				g2.play(choice(g2.actionSpace()))
 
 			totals[c1] += 1
-			if g3.winner == me:
-				wins[c1] += 1
+			wins[c1] += (g2.winner == me)
 
 			it += 1
-			if it & (128 - 1) == 1 and timer() - start > timeout:
-				if verbose: print('%d iters (~%f turns ahead)' % (it, math.log(it, len(cs))))
+			if it & (32 - 1) == 0 and timer() - start > timeout:
+				dt = timer() - start
+				if verbose: print('%d rollouts in %.3fs (~%.3f rol/s; ~%.3f turns ahead)' % (it, dt, it/dt, math.log(it, len(cs))))
 				break
 
 		scores = {c: wins[c] / totals[c] for c in cs}
-		if verbose: print(scores)
+		if verbose: print('{' + ', '.join('%d: %.2f%%' % (k, v * 100) for k, v in sorted(scores.items(), key=lambda t: -t[1])) + '}')
 		if verbose: print('Win prob: %.2f%%' % (max(scores.values()) * 100))
 		return out(max(scores, key=scores.get))
 
 	return f
 
 g = Game()
-
 mode = 'play'
-
 if len(sys.argv) > 1:
 	mode = sys.argv[1]
-	if mode == 'play':
-		if len(sys.argv) > 2 and sys.argv[2] == 'human':
-			players = {1: human(), 2: mcs(timeout=5.0)}
-	elif mode == 'stats':
-		wins = {1: 0, 2: 0, 3: 0}
 
-		if len(sys.argv) > 2:
-			wins[1] = int(sys.argv[2])
-		if len(sys.argv) > 3:
-			wins[2] = int(sys.argv[3])
-		if len(sys.argv) > 4:
-			wins[3] = int(sys.argv[4])
-	elif mode == 'replay':
-		moves = list(map(int, sys.argv[2]))
 
 if mode == 'play':
-	players = {1: mcs(timeout=5.0), 2: human()}
-	print(g)
+	timeout = 2.0
+	# players = {1: mcs(timeout=timeout), 2: human()}
+	players = {1: mcs(timeout=5.0), 2: mcs(timeout=5.0)}
+	# players = {1: smartNoise(), 2: smartNoise()}
+	if len(sys.argv) > 2 and sys.argv[2] == 'human':
+		players = {1: human(), 2: mcs(timeout=timeout)}
+
 	while not g.winner:
+		print(g)
+		print()
 		print(g.P[g.p], end=' ')
 		mv = players[g.p](g)
 		if not g.canPlay(mv):
 			continue
 		g.play(mv)
-		print(g)
+		print()
+	print(g)
+
 
 elif mode == 'stats':
+	wins = {1: 0, 2: 0, 3: 0}
+	if len(sys.argv) > 2:
+		wins[1] = int(sys.argv[2])
+	if len(sys.argv) > 3:
+		wins[2] = int(sys.argv[3])
+	if len(sys.argv) > 4:
+		wins[3] = int(sys.argv[4])
+
 	timeout = 10.0
 	players = {1: mcs(timeout=timeout, verbose=False), 2: mcs(timeout=timeout, verbose=False)}
 
@@ -275,7 +287,10 @@ elif mode == 'stats':
 		t += 1
 		print('%d\t[%.3fs]\t%d wins (1: %d (%.1f%%), 2: %d (%.1f%%), draw: %d (%.1f%%))\t%s' % (t, end - start, g.winner, wins[1], wins[1]/t*100, wins[2], wins[2]/t*100, wins[3], wins[3]/t*100, ''.join(map(str, seq))))
 
+
 elif mode == 'replay':
+	moves = list(map(int, sys.argv[2]))
+
 	print('Replaying with %s' % moves)
 	print(g)
 
